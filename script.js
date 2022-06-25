f3297ba3e239d8b8ec6edc493a0d2918f7bb193b
Sameep <sameepsawant10@gmail.com>
2022-06-26 00:25:51 +0530

    added the form table

--- script.js
+++ script.js
@@ -4,20 +4,28 @@ import groceryItemsList from "/Items.js";
 //Getting the elements using id's
 const groceryDiv = document.getElementById("groceryList");
 
+const getId = (name) => {
+  console.log(name);
+};
+
 //displaying the groceryItems list.
 groceryItemsList.map((item) => {
-  console.log(item.url);
   groceryDiv.innerHTML += `
     <div class="p-10 border border-2 rounded-lg hover:shadow-lg cursor-pointer">
         <img src="${item.url}" alt="product name" class="h-[200px] mx-auto"/>
         <p>${item.name}</p>
         <p>RS. ${item.price}</p>
-        <div class="flex justify-between">
-        <button class="p-5">-</button>
-        <p class="flex justify-center items-center">10</p>
-        <button class="p-5">+</button>
-        </div>
+        <button id="${item.name}">Increase Quantity</button>
         <button class="w-full block mx-auto px-10 bg-red-200">Add To Cart</button>
     </div>
     `;
 });
+
+//Increment button.
+// <div class="flex justify-between">
+// <button class="p-5" onclick="decrementItemsQuantity(itemQuantity)">-</button>
+// <p class="flex justify-center items-center">${itemQuantity}</p>
+// <button class="p-5" id="${item.name}">+</button>
+// </div>
+
+// ${incrementShow()}
