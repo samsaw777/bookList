6b258cd02a2db915d4a27ca6ea6ce672f6f935c1
Sameep <sameepsawant10@gmail.com>
2022-07-01 21:09:37 +0530

    id check added into the code

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // Book class to create a Book.
 class Book {
-  constructor(bookName, bookAuthor) {
+  constructor(bookName, bookAuthor, bookId) {
     this.bookName = bookName;
     this.bookAuthor = bookAuthor;
+    this.bookId = bookId;
   }
 }
 
@@ -26,6 +27,7 @@ class UI {
     row.innerHTML = `
     <td class="py-3 border-b border-gray-400 bg-white">${book.bookName}</td>
     <td class="py-3 border-b border-gray-400 bg-white">${book.bookAuthor}</td>
+    <td class="py-3 border-b border-gray-400 bg-white">${book.bookId}</td>
     <td class="py-3 border-b border-gray-400 bg-white ">
       <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 cursor-pointer text-red-600 mx-auto delete" viewBox="0 0 20 20" fill="currentColor">
         <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
@@ -56,14 +58,14 @@ class UI {
   static clearFileds() {
     document.getElementById("bookName").value = "";
     document.getElementById("bookAuthor").value = "";
+    document.getElementById("bookId").value = "";
   }
 
   //Deleting the books.
   static deleteBook(element) {
-    console.log(element.parentElement);
+    console.log();
     if (element.parentElement.classList.contains("delete")) {
       element.parentElement.parentElement.parentElement.remove();
-      console.log("Deleting");
     }
   }
 }
@@ -89,12 +91,11 @@ class LocalStore {
     localStorage.setItem("books", JSON.stringify(books));
   }
 
-  static removeBook(bookName) {
+  static removeBook(bookId) {
     const books = LocalStore.getBooks();
 
     books.forEach((book, index) => {
-      if (bookName === book.bookName) {
-        console.log(true);
+      if (bookId === book.bookId) {
         books.splice(index, 1);
       }
     });
@@ -114,24 +115,41 @@ document.getElementById("bookForm").addEventListener("submit", (e) => {
   // Getting the book values.
   const bookName = document.getElementById("bookName").value;
   const bookAuthor = document.getElementById("bookAuthor").value;
+  const bookId = document.getElementById("bookId").value;
 
   //Validate the fields
-  if (bookName === "" || bookAuthor === "") {
+  if (bookName === "" || bookAuthor === "" || bookId === "") {
     UI.showAlert("Please Fill all the fileds!", "red");
   } else {
-    // Creating a object of the Book.
-    const newBook = new Book(bookName, bookAuthor);
+    // Get the books from the local storage and check if book ID is already present in it.
+    const books = LocalStore.getBooks();
+    const isPresent = false;
 
-    // Adding the book to the UI.
-    UI.addBookToList(newBook);
+    books.forEach((book, index) => {
+      if (bookId === book.bookId) {
+        UI.showAlert(
+          "Cannot add book as the ID is present in the List!",
+          "red"
+        );
+        isPresent = true;
+      }
+    });
 
-    //Add book to the store.
-    LocalStore.addBook(newBook);
+    if (!isPresent) {
+      // Creating a object of the Book.
+      const newBook = new Book(bookName, bookAuthor, bookId);
 
-    UI.showAlert("Submited Book!", "green");
+      // Adding the book to the UI.
+      UI.addBookToList(newBook);
 
-    //Cleaning the fields.
-    UI.clearFileds();
+      //Add book to the store.
+      LocalStore.addBook(newBook);
+
+      UI.showAlert("Submited Book!", "green");
+
+      //Cleaning the fields.
+      UI.clearFileds();
+    }
   }
 });
 
@@ -142,8 +160,7 @@ document.getElementById("bookList").addEventListener("click", (e) => {
 
   //Removing book from localstorage.
   LocalStore.removeBook(
-    e.target.parentElement.parentElement.previousElementSibling
-      .previousElementSibling.textContent
+    e.target.parentElement.parentElement.previousElementSibling.textContent
   );
 
   // console.log(
